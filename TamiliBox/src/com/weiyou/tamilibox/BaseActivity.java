0cdb8dbfe7b228d02c4f8419863cc28eeb8a2529
Raptor <Raptor>
2016-06-09 21:02:20 +0800

    update framework

--- TamiliBox/src/com/weiyou/tamilibox/BaseActivity.java
+++ TamiliBox/src/com/weiyou/tamilibox/BaseActivity.java
@@ -1,10 +1,24 @@
 package com.weiyou.tamilibox;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class BaseActivity extends Activity {
+
+	public final static String OPRATION_INTENT_FILTER = "WEIYOU_OPERATION_INTENT_FILTER";
+	
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +27,29 @@ public class BaseActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
 	}
 
+	protected void onCommondBack() {
+		
+	}
+
+	protected void onCommondNext() {
+	}
+
+	protected void onCommondPrev() {
+		
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(mReceiver, new IntentFilter(OPRATION_INTENT_FILTER));
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(mReceiver);
+	}
+
+
+	
 }
